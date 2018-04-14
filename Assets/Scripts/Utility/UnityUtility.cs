01f82617d34a6824bc9ebea84d3127a00a2b8378
JBeto <jbeto001@ucr.edu>
2018-04-13 21:30:57 -0700

    Added folder structure and utility script

---
+++ Assets/Scripts/Utility/UnityUtility.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UnityUtility
+{
+    public static T GetSafeComponent<T>(GameObject gameObject)
+    {
+        T component = gameObject.GetComponent<T>();
+        if (component == null)
+        {
+            Debug.LogError("Component requested is null!");
+        }
+        return component;
+    }
+}
