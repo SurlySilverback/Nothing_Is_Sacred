e99c1a581885d1478607745019b503229a464a6d
Gabriel <ironphoenix@gmail.com>
2018-05-01 12:12:42 -0700

    Committing so that I can pull to fix compiler errors from other team mate assets. Testing abstract class instances of Unit class.

--- Assets/Gabriel/Scripts/Unit.cs
+++ Assets/Gabriel/Scripts/Unit.cs
@@ -2,28 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit {
-
-	private:
-
-	float speed;
-	int heat;
-	int subtlety;
-	List<Goods> inventory;
-
-	float x_coord;
-	float y_coord;
-}
-
-public class NewBehaviourScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+[CreateAssetMenu()]
+public class Unit : ScriptableObject
+{
+	[SerializeField]
+	protected float baseSpeed;				// Determines the base speed of the unit on roads.
+	public int Heat {get; set;}					// Determines how aggressively the government will chase after this unit.
+	public int Subtlety {get; set;}				// Determines how effective the unit is at avoiding detection.
+	public Vector2 Coordinates {get; set;}			// The unit's x-y coordinates on the map. 
+	public Vector2Int inventorySize;		// Determines the max capacity carryable by the unit.
+	public List<Goods> Inventory;			// The Goods currently carried by the inventory.
 }
