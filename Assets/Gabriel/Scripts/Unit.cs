b83cab02cfdac1dd49b249ddcb1e4f78e98b66f1
Gabriel <ironphoenix@gmail.com>
2018-05-01 11:12:52 -0700

    First proper commit

---
+++ Assets/Gabriel/Scripts/Unit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit {
+
+	private:
+
+	float speed;
+	int heat;
+	int subtlety;
+	List<Goods> inventory;
+
+	float x_coord;
+	float y_coord;
+}
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
