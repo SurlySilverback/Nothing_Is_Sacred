e66aecee3617984b5e699548dff6fac13c56a99b
JBeto <jbeto001@ucr.edu>
2018-05-01 18:57:05 -0700

    Created basic map

---
+++ Assets/JBeto/Tilemaps/Cities.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Cities" tilewidth="400" tileheight="280" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="City">
+  <image width="400" height="280" source="../../Art/city.png"/>
+  <objectgroup draworder="index">
+   <object id="2" type="City" x="0" y="0" width="400" height="280"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../../Art/Black.png"/>
+ </tile>
+</tileset>
