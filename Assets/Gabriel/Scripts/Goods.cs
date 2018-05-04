64185cb1437e54a28998b2de6ba5477ced62d4d2
Gabriel <ironphoenix@gmail.com>
2018-05-04 11:19:46 -0700

    Committing to pull new changes.

--- Assets/Gabriel/Scripts/Goods.cs
+++ Assets/Gabriel/Scripts/Goods.cs
@@ -12,10 +12,11 @@ public class Goods : ScriptableObject
 												// happiness and self-fulfillment) will be pursued. maslowIndex indicates which
 												// level of the urgency hierarchy the good satisfies, thereby determining its
 												// selling price under certain circumstances. THIS VALUE DOES NOT CHANGE.
+												// THIS VALUE IS NOT DISPLAYED TO THE PLAYER.
 	[SerializedField]
 	protected int scarcity;						// Measures the "base rarity" of the Good. Scarce goods have a higher base price,
 												// and their price increases in greater increments as their local supply diminishes.
-												// THIS VALUE DOES NOT CHANGE.
+												// THIS VALUE DOES NOT CHANGE. THIS VALUE IS NOT DISPLAYED TO THE PLAYER.
 
 	[SerializedField]
 	protected int basePrice;					// basePrice is the lowest price that a Good can be traded for. It is a reflection of 
@@ -25,12 +26,13 @@ public class Goods : ScriptableObject
 												// or if it will be used directly to calculate prices, basePrice is being created to serve
 												// that role in the interim. If we can design the game so that scarcity alone can be used 
 												// to determine the prices, then basePrice variable will be discarded. THIS VALUE DOES NOT CHANGE.
+												// THIS VALUE IS NOT DISPLAYED TO THE PLAYER.
 
 	[SerializedField]
 	protected int baseHeat;						// The heat value of goods may increase or decrease over time, but certain goods will
 												// always have a minimum heat value. baseHeat represents the minimum heat value a good 
 												// will ever have. For example, Weapons will always have a higher baseHeat value 
-												// than Food. THIS VALUE DOES NOT CHANGE.
+												// than Food. THIS VALUE DOES NOT CHANGE. THIS VALUE IS NOT DISPLAYED TO THE PLAYER.
 
 	public int Heat {get; set;}					// Determines how aggressively the government pursues Goods of this type. The Heat value
 												// of a Good determines how much a Unit's heat value will increase when the Unit is detected
@@ -38,4 +40,8 @@ public class Goods : ScriptableObject
 
 												// #futurefeature: The total heat value of a Unit's carried goods will determine the "sentence" 
 												// of a Unit when it is captured by government forces. Prison for low heat total, execution for high heat.
+												// THIS VALUE IS DISPLAYED IN THE MENU.
+
+	public int Tyranny {get; set;}				// Determines how much the government's Tyranny increases when the player sells this Good to the
+												// government.
 }
