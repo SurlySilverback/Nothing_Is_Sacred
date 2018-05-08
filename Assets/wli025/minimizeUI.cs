24b192e24a0561d4c9cf91392dc6eb5bb660e56f
Wendy <wli025@ucr.edu>
2018-05-08 10:50:54 -0700

    drag and drop works, highlight inventory slot, sliders added but not functionality

---
+++ Assets/wli025/minimizeUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// public class minimizeUI : MonoBehaviour {
+
+// 	// Use this for initialization
+// 	void Start () {
+		
+// 	}
+	
+// 	// Update is called once per frame
+// 	void Update () {
+		
+// 	}
+// }
+ 
+ public class MoveButton : MonoBehaviour {
+ 
+	//Get Parent Object
+    // childObject.transform.parent.gameObject;
+    // public bool isUp = true;
+
+    // void minimizeToggle(){
+    //     if(isUP){
+
+    //         isUP = false;
+    //         print ("first if");
+    //     }
+    //     else{
+    //         isUP = true;
+    //         print ("second if");
+    //     }
+    // }
+         
+ }
