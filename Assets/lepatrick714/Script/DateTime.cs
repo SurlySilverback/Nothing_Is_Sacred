0b92f9e6bfa63004b76754293c0addfdd8093979
lepatrick714 <lepatrick714@gmail.com>
2018-05-18 14:49:19 -0700

    adding initial datetime cs

---
+++ Assets/lepatrick714/Script/DateTime.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DateTime : MonoBehaviour 
+{
+	private System.DateTime moment; 
+	private float timePassed; 
+
+	// Getters
+	public static System.DateTime getDate() { 
+		return this.moment; 
+	}
+
+
+
+
+
+
+
+
+	// One second of Real time = One Minute of In Game Time 
+	void UpdateMin() 
+	{ 
+		this.moment.AddMinutes(1);
+	}
+
+	
+	// Update is called once per frame
+	void Awake()
+	{
+		this.moment = new System.DateTime(2013, 9, 15, 12, 0, 0);
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	void Update () 
+	{	
+		this.timePassed += Time.deltaTime; 
+		if (this.timePassed >= 1) 
+		{ 
+			UpdateMin(); 
+			timePassed -= 1; 
+		}
+	}
+}
