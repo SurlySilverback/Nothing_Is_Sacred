bc8fc5ccc7d2e383882f644df2fdf30601b141dc
lepatrick714 <lepatrick714@gmail.com>
2018-05-22 19:20:08 -0700

    completed red circle controller

---
+++ Assets/lepatrick714/Script/HeatCircleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatCircleController : MonoBehaviour {
+	// Max Heat Numerical Value is 1000 
+	float maxHeat = 10; 
+	public City city; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Debug.Log(city.GetHeat());
+		float size_of_circle = city.GetHeat() / 100;  
+		transform.localScale = new Vector3(size_of_circle, size_of_circle, 0);
+		// if(city.GetHeat() < 1000) { 
+			// city.SetHeat(city.GetHeat() + 10); 
+		// }
+	}
+}
