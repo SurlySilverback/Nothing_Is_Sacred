27c07b31c0ef566746ab5134de4642e5f08539d2
JBeto <jbeto001@ucr.edu>
2018-06-01 21:55:09 -0700

    Pushed

--- Assets/Tilemaps/Cities.tsx
+++ Assets/Tilemaps/Cities.tsx
@@ -2,7 +2,7 @@
 <tileset name="Cities" tilewidth="4566" tileheight="2895" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="1" type="City">
-  <image width="2831" height="1569" source="../wli025/UI_Images/mapImages/city.png"/>
+  <image width="2831" height="1569" source="../Art/Tileset/Cities/city.png"/>
   <objectgroup draworder="index">
    <object id="2" x="0" y="1060.67">
     <polygon points="0,0 1391.67,-1041.67 2808.33,-33.3333 2800,483.333 16.6667,483.333"/>
@@ -10,7 +10,7 @@
   </objectgroup>
  </tile>
  <tile id="2" type="City">
-  <image width="4566" height="2895" source="../wli025/UI_Images/mapImages/capital.png"/>
+  <image width="4566" height="2895" source="../Art/Tileset/Cities/capital.png"/>
   <objectgroup draworder="index">
    <object id="1" x="16" y="2320">
     <polygon points="0,0 2736,-1520 4512,16 4512,480 16,496"/>
@@ -18,7 +18,7 @@
   </objectgroup>
  </tile>
  <tile id="3" type="City">
-  <image width="2769" height="1534" source="../wli025/UI_Images/mapImages/snowyCity.png"/>
+  <image width="2769" height="1534" source="../Art/Tileset/Cities/snowyCity.png"/>
   <objectgroup draworder="index">
    <object id="3" x="0" y="0" width="2758.33" height="1508.33"/>
    <object id="4" x="-8.33333" y="975">
